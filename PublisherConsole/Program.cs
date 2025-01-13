89fc92dcccaf4363799ac16fdee1234be1d161e1
victor prado <vmpradob@gmail.com>
2025-01-13 15:53:46 -0400

    testing insertions and retrival of data with ORM

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PublisherData;
+using PublisherDomain;
+
+using (PubContext context=new PubContext())
+{
+    context.Database.EnsureCreated();
+}
+
+GetAuthors();
+AddAuthor();
+GetAuthors();
+
+void AddAuthor()
+{
+    using (PubContext context = new PubContext())
+    {
+        Author author = new Author
+        {
+            FirstName = "Mozart",
+            LastName = "La Para"
+        };
+        context.Authors.Add(author);
+        context.SaveChanges();
+    }
+}
+
+
+void GetAuthors()
+{
+    using (PubContext context = new PubContext())
+    {
+        var authors = context.Authors.ToList();
+        foreach (var author in authors)
+        {
+            Console.WriteLine(author.FirstName + " " + author.LastName);
+        }
+    }
+}
